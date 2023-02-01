d3620d93d69c5a91129fa9f2ccf99b0435cb5896
semin1012 <73764069+semin1012@users.noreply.github.com>
2023-02-01 14:16:00 +0900

    directory

---
+++ GameProject/Source/GameProject/Inventory/InventoryMenuUW.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryMenuUW.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPROJECT_API UInventoryMenuUW : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
