e02212b181d16e4719614e4e1c381a13c7353f41
Woo Jeong Yeon <kikanaidek@gmail.com>
2023-03-15 14:59:06 +0900

    adding Enemy AI
    EnemyBT
    EnemyBB
    EnemyCharacter
    EnemyAIController

---
+++ GameProject/Source/GameProject/EnemyAIController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyAIController.h"
+#include "EnemyCharacter.h"
+#include "BehaviorTree/BehaviorTree.h"
+
+AEnemyAIController::AEnemyAIController()
+{
+	BehaviorComp = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("BehaviorComp"));
+	BlackboardComp = CreateDefaultSubobject<UBlackboardComponent>(TEXT("BlackboardComp"));
+}
+
+void AEnemyAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	
+	// InPawn을 EnemyCharacter로 변환
+	auto Char = Cast <AEnemyCharacter>(InPawn);
+	// 포인터가 유효한지 검사
+	if(Char && Char->EnemyBehaviorTree)
+	{
+		BlackboardComp->InitializeBlackboard(*Char->EnemyBehaviorTree->BlackboardAsset);
+		TargetKeyID = BlackboardComp->GetKeyID("Target");
+		BehaviorComp->StartTree(*Char->EnemyBehaviorTree);
+	}
+}
