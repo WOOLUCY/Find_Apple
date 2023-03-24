c37337a617939079c981f067b7e5f7c5d7c15a40
semin1012 <semin1012@gmail.com>
2023-03-24 15:28:54 +0900

    add Crafting Widget
    제작 관련 위젯들 cpp, h 파일 생성만 함

---
+++ GameProject/Source/GameProject/Crafting/CraftingAllWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CraftingAllWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPROJECT_API UCraftingAllWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ GameProject/Source/GameProject/Crafting/CraftingItemWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CraftingItemWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPROJECT_API UCraftingItemWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
