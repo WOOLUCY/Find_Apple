67309eb5acb00318945e287f4c9c97732fa3bf48
Woo Jeong Yeon <kikanaidek@gmail.com>
2023-05-06 20:28:54 +0900

    Manage Behavior Tree Tasks

---
+++ GameProject/Source/GameProject/BTTask/BTTask_ChestAttack.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_ChestAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPROJECT_API UBTTask_ChestAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_ChestAttack();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+};
