cc740a5de459b2a8fc384450f320da2fa989d04b
semin1012 <semin1012@gmail.com>
2023-05-12 11:25:46 +0900

    Update InventoryDataTable.h

--- GameProject/Source/GameProject/Inventory/InventoryDataTable.h
+++ GameProject/Source/GameProject/Inventory/InventoryDataTable.h
@@ -15,6 +15,9 @@ struct FInventoryTableRow : public FTableRowBase
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	int32 KindOfItem = 0;	// 서버 정보 주고 받기 위한 숫자. 0부터 1씩 증가하는 아이템별 식별 번호
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
 	int32 ItemType = 0;		/* 먹을 수 있으면 1 / eatable = 1 / not visible = 2 */
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
@@ -40,6 +43,10 @@ public:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
 	int BeginningHaveCount = 0;
+
+	// 서버에게 보낼 수 있는 아이템인지 (true = 보낼 수 있음)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	bool CanSendToServer= true;
 
 };
 
